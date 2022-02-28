8078b6534b3a698bf3ae3960b16187537a3e04ee
Nico Picchio <nicolapicchio@gmail.com>
2022-02-28 15:06:00 +0000

    minor refactor

--- src/app.js
+++ src/app.js
@@ -1,70 +1,65 @@
-import Header from './components/header'
+import Header from './components/header';
 
-import initialEmails from './data/emails'
+import initialEmails from './data/emails';
 
-import './styles/app.css'
+import './styles/app.css';
 
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [inbox, setInbox] = useState(initialEmails)
-  
-  return (
-    <div className="app">
-      <Header />
-      <nav className="left-menu">
-        <ul className="inbox-list">
-          <li
-            className="item active"
-            // onClick={() => {}}
-          >
-            <span className="label">Inbox</span>
-            <span className="count">?</span>
-          </li>
-          <li
-            className="item"
-            // onClick={() => {}}
-          >
-            <span className="label">Starred</span>
-            <span className="count">?</span>
-          </li>
+	const [inbox, setInbox] = useState(initialEmails);
 
-          <li className="item toggle">
-            <label for="hide-read">Hide read</label>
-            <input
-              id="hide-read"
-              type="checkbox"
-              checked={false}
-              // onChange={() => {}}
-            />
-          </li>
-        </ul>
-      </nav>
-      <main className="emails">
-        <ul>
-          {inbox.map(function(email) {
-            return (
-            <li className="email">
-            <div className="select">
-            <input
-              className="select-checkbox"
-              type="checkbox"/>
-            </div>
-            <div className="star">
-            <input
-              className="star-checkbox"
-              type="checkbox"
-            />
-            </div>
-            <div className="sender">{email.sender}</div>
-            <div className="title">{email.title}</div>
-            </li>
-            )
-          })}
-        </ul>
-      </main>
-    </div>
-  )
+	return (
+		<div className="app">
+			<Header />
+			<nav className="left-menu">
+				<ul className="inbox-list">
+					<li
+						className="item active"
+						// onClick={() => {}}
+					>
+						<span className="label">Inbox</span>
+						<span className="count">?</span>
+					</li>
+					<li
+						className="item"
+						// onClick={() => {}}
+					>
+						<span className="label">Starred</span>
+						<span className="count">?</span>
+					</li>
+
+					<li className="item toggle">
+						<label for="hide-read">Hide read</label>
+						<input
+							id="hide-read"
+							type="checkbox"
+							checked={false}
+							// onChange={() => {}}
+						/>
+					</li>
+				</ul>
+			</nav>
+			<main className="emails">
+				<ul>
+					{inbox.map((email) => {
+						return (
+							<li className="email">
+								<div className="select">
+									<input className="select-checkbox" type="checkbox" />
+								</div>
+								<div className="star">
+									<input className="star-checkbox" type="checkbox" />
+								</div>
+								<div className="sender">{email.sender}</div>
+								<div className="title">{email.title}</div>
+							</li>
+						);
+					})}
+				</ul>
+			</main>
+		</div>
+	);
 }
 
-export default App
+export default App;
